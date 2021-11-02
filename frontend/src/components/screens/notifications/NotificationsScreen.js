f45f71889074ae5901bdd1c4eed2d8c9e36740ea
rfhic-code <35171554+htkim298@users.noreply.github.com>
2021-11-02 19:55:47 +0900

    [수정] 알림 이름 수정

--- frontend/src/components/screens/notifications/NotificationsScreen.js
+++ frontend/src/components/screens/notifications/NotificationsScreen.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function AlarmScreen() {
+export default function Notifications() {
     return (
-        <View>
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
             <Text></Text>
         </View>
     )
